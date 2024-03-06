83f942b5ca4b603a1f979c6b097e5f004ff4d153
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-03-06 11:09:22 +0000

    aaaa

--- Exercicio003.c
+++ Exercicio003.c
@@ -7,21 +7,27 @@ struct data {
 int compara_datas(struct data d1, struct data d2);
 
 int compara_datas(struct data d1, struct data d2) {
-  if(d1.dia && d1.mes && d1.ano == d2.dia && d2.mes && d2.ano){
+  if(d1.dia == d2.dia && d1.mes == d2.mes && d1.ano == d1.ano){
     return 0;
-  }
-  else if(d1.dia && d1.mes && d1.ano < d2.dia && d2.mes && d2.ano){
+  } else if((d1.ano < d2.ano) || (d1.ano == d2.ano && d1.mes < d2.mes) || (d1.ano == d2.ano && d1.mes == d2.mes && d1.dia < d2.dia)){
     return -1;
-  }
-  
-  else if(d1.dia && d1.mes && d1.ano > d2.dia && d2.mes && d2.ano){
+  } else if((d1.ano > d2.ano) || (d1.ano == d2.ano && d1.mes > d2.mes) || (d1.ano == d2.ano && d1.mes == d2.mes && d1.dia > d2.dia)){
     return 1;
-  }
+  } //else if(d1.ano == d2.ano && d1.mes < d2.mes){
+    //return -1;
+ // } else if(d1.ano == d2.ano && d1.mes > d2.mes){
+  //  return 1;
+ // } //else if(d1.ano == d2.ano && d1.mes == d2.mes && d1.dia < d2.dia){
+    //return -1;
+  //} else if(d1.ano == d2.ano && d1.mes == d2.mes && d1.dia > d2.dia){
+  //  return 1;
+ // } 
 }
 
 // 20 1 1980 21 3 1979
 // 20 1 1979 21 3 1980
 //  5 8 2022 5 8 2022
+//  21 3 1980 20 1 1980
 
 int main() {
   struct data d1, d2;
