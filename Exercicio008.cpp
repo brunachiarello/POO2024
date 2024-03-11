6d91bb0c0f1eb4e758cefeae0c70d636c246eac9
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-03-11 19:00:49 +0000

    aaaaaaaa

--- Exercicio008.cpp
+++ Exercicio008.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
 using namespace std;
+/*Fac ¸a um programa em C++ que leia um vetor com nove n´ umeros inteiros, calcule e mostre os n´ umeros primos e
+ suas respectivas posic ¸˜ oes.*/
+
+//int primo(int vetor[9]){
+
+    
+    //return 1;
+
+/*for(int i = 0; i < 9; i++){
+       if(vetor % 0 = vetor[i]){
+            cout << vetor[i] << i << endl;
+        } 
+    }
+*/
+    
+
 
 int main(){
+    int vetor[9];
+    for(int i=0; i<9; i++){
+        cin >> vetor[i];
+    }
+
+    for(int i = 1; i < 1000; i++){
+        for(int j = 0; j < 9; j++){
+            if(vetor[j] % i == 0){
+                cout << "Não tem primos" << endl;
+            } else {
+                cout << vetor[j] << j << endl;
+            }
+        }
+    }
+
+
+
     return 0;
 }
--- Exercicio009.cpp
+++ Exercicio009.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
+int imprime(int matriz[6][4]){
+    for(int l = 0; l < 6; l++){ 
+        for(int c = 0; c < 4; c++){ 
+            cout << matriz[l][c] << endl;
+        }
+    } return 1;
+}
+
+int calcula_matriz(int matriz[6][4]){
+    int maior = INT_MIN; 
+    for(int l = 0; l < 6; l++){
+        for(int c = 0; c < 4; c++){
+            if(c < maior){
+                maior = c;
+                matriz[l][c] = matriz[l][c] * maior;
+                
+            }
+        } 
+    }
+    return imprime(matriz);
+}
+
+
+
 int main(){
+    int matriz[6][4];
+
+    for(int l = 0; l < 6; l++){ // l = linha
+        for(int c = 0; c < 4; c++){ // c = coluna
+            //cout << "Lendo matriz..." << endl;
+            cin >> matriz[l][c];
+        }
+    }
+
+    calcula_matriz(matriz);
+    
     return 0;
 }
