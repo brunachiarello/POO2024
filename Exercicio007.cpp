b79338b0138d1ec162724201598c0a4cf9162981
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-03-12 17:01:46 +0000

    aaaa

--- Exercicio007.cpp
+++ Exercicio007.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 double calcula_media(int idade[10]){ // media das idades das dez pessoas
-    double soma; 
+    double soma = 0; 
     for(int i = 0; i < 10; i++){
         if(i < 10){
-           soma = idade[i] + idade[i + 1]; 
+           soma += idade[i]; 
         }
     }
     return soma/10;
@@ -15,21 +16,30 @@ double calcula_media(int idade[10]){ // media das idades das dez pessoas
 int calcula_quant(double peso[10], double altura[10]){ // quantidade de pessoas com peso superior a 90 kg e altura inferior a 1,50 metro
     int quant = 0;
     for(int i = 0; i < 10; i++){
-        if(peso[i] > 90.0 && altura[i] > 1,5){
-            quant++;
-        }
+        if(peso[i] > 90.0 && altura[i] < 1.5){
+        quant++;
+        } 
     }
-    return quant;
+    return quant; 
 }
 
 double calcula_pcent(int idade[10], double altura[10]){ // porcentagem de pessoas com idade entre 10 e 30 anos entre as pessoas que medem mais de 1,90m
-    double total; 
+    double total_altura = 0;
+    double total_idade = 0;
+   
     for(int i = 0; i < 10; i++){
-        if(idade[i] > 1,9 && idade[i] >= 10 && idade[i] <= 30){
-            total++;
+        if(altura[i] > 1.9){
+            total_altura++;
+        }
+        if(altura[i] > 1.9 && idade[i] >= 10 && idade[i] <= 30){
+            total_idade++;
         }
     }
-    return total/100;
+
+    if(total_altura != 0){
+        return (total_idade / total_altura) * 100; // pessoas de 1,90 é o total 
+    }
+    return 0;
 }
 
 int main(){
@@ -41,6 +51,6 @@ int main(){
         cin >> idade[i] >> peso[i] >> altura[i];
     }
 
-    cout << calcula_media(idade) << " " << calcula_quant(peso, altura) << " " << calcula_pcent(idade, altura);
+    cout << fixed << setprecision(4) << calcula_media(idade) << " " << calcula_quant(peso, altura) << " " << calcula_pcent(idade, altura) << endl;
     return 0;
 }
--- Exercicio009.cpp
+++ Exercicio009.cpp
@@ -3,26 +3,27 @@
 
 using namespace std;
 
-int imprime(int matriz[6][4]){
-    for(int l = 0; l < 6; l++){ 
-        for(int c = 0; c < 4; c++){ 
-            cout << matriz[l][c] << endl;
-        }
-    } return 1;
-}
 
-int calcula_matriz(int matriz[6][4]){
+void calcula_matriz(int matriz[6][4]){
     int maior = INT_MIN; 
+    
     for(int l = 0; l < 6; l++){
         for(int c = 0; c < 4; c++){
-            if(c < maior){
-                maior = c;
+            if(matriz[l][c] < maior){
+                maior = matriz[l][c];
                 matriz[l][c] = matriz[l][c] * maior;
                 
             }
         } 
     }
-    return imprime(matriz);
+    
+   for(int l = 0; l < 6; l++){ 
+        for(int c = 0; c < 4; c++){
+           cout << matriz[l][c] << " "; 
+        } 
+        cout << endl;
+    } 
+//    return 1;
 }
 
 
@@ -30,9 +31,9 @@ int calcula_matriz(int matriz[6][4]){
 int main(){
     int matriz[6][4];
 
+    // Lê matriz
     for(int l = 0; l < 6; l++){ // l = linha
         for(int c = 0; c < 4; c++){ // c = coluna
-            //cout << "Lendo matriz..." << endl;
             cin >> matriz[l][c];
         }
     }
