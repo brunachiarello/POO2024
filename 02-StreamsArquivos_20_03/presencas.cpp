6d111515a97d5277f0c23a15cf08bd3b3a9a579b
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-03-20 11:59:16 +0000

    aula 20/03

---
+++ 02-StreamsArquivos_20_03/presencas.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define MAX_PRESENCAS 80
+
+using namespace std;
+
+class Aluno{
+private:
+    string nome;
+    char presencas[MAX_PRESENCAS];
+    int numPresencas;
+public:
+    Aluno(string n = "");
+    ~Aluno();
+    string obtemNome();
+    void defineNome(string n);
+    bool adcionaPresanca(char p){
+        if(numPresencas >= MAX_PRESENCAS){
+            return false;
+        }
+        presencas[numPresencas++] = p;
+        return true;
+    }
+    char obtemPresenca(int i);
+    int obtemNumPresencas();
+    string str();
+};
+
+int main(){
+    return 0;
+}
