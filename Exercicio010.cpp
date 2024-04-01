241c4e87b551459ee868078f2dc6b3b18e300665
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-04-01 12:29:58 +0000

    zzzzzz

--- Exercicio010.cpp
+++ Exercicio010.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -19,11 +20,9 @@ bool anoBissexto(Data data){
 void dataValida(Data data[5]){
     int dataInvalida = 0;
     Data dataValida[5]; // datas válidas são armazenadas
-
-    for (int a = 0; a < 5; a++){
-
-        for (int i = 0; i < 5; i++){
-
+    int a = 0;
+    for (int i = 0; i < 5; i++){
+        
             if (data[i].dia >= 1 && data[i].dia <= 31 && data[i].mes >= 1 && data[i].mes <= 12){ // mes e dia válido 
 
                 if (data[i].mes == 2){ // mes é fevereiro
@@ -32,6 +31,8 @@ void dataValida(Data data[5]){
                         if (data[i].dia == 29){
                             cout << "+" << endl;
                             dataValida[a] = data[i];
+                            a++;
+                            break;
                         } else {
                             cout << "-" << endl;
                             dataInvalida++;
@@ -41,6 +42,7 @@ void dataValida(Data data[5]){
                         if (data[i].dia <= 28){
                             cout << "+" << endl;
                             dataValida[a] = data[i];
+                            a++;
                         } else {
                             cout << "-" << endl;
                             dataInvalida++;
@@ -52,6 +54,7 @@ void dataValida(Data data[5]){
                         if (data[i].dia <= 30){ 
                             cout << "+" << endl;
                             dataValida[a] = data[i];
+                            a++;
                         } else {
                             cout << "-" << endl;
                             dataInvalida++;
@@ -63,6 +66,7 @@ void dataValida(Data data[5]){
                         if (data[i].dia <= 31){
                             cout << "+" << endl;
                             dataValida[a] = data[i];
+                            a++;
                         } else {
                             cout << "-" << endl;
                             dataInvalida++;
@@ -74,31 +78,49 @@ void dataValida(Data data[5]){
                 cout << "-" << endl;
                 dataInvalida++;
             }
-        }  
+          
     }        
 
     if (dataInvalida == 5){ // 5 datas inválidas
             cout << "*" << endl;
 
-        } else { // algumas datas válidas
 
-            Data maisAntiga; // vai receber a data mais antiga
+    } else { // algumas datas válidas
 
-            for(int k = 0; k < 5; k++){ // loop para ver entre as datas validas
-                if (dataValida[k].ano < dataValida[k + 1].ano){
+        Data maisAntiga; // vai receber a data mais antiga
+        maisAntiga = dataValida[0]; // coloca a primeira válida como mais antiga
+
+        for(int k = 1; k < a; k++){ // loop para ver entre as datas validas
+            if (dataValida[k].ano < maisAntiga.ano){ // anos diferentes
+                maisAntiga = dataValida[k];
+            } else if (dataValida[k].ano == maisAntiga.ano){ // anos iguais
+                if(dataValida[k].mes < maisAntiga.mes){  // mes diferente
                     maisAntiga = dataValida[k];
-                } else if (dataValida[k].ano == dataValida[k + 1].ano){ // anos iguais
-                    if(dataValida[k].mes < dataValida[k + 1].mes){ 
+                } else if (dataValida[k].mes == maisAntiga.mes){ // mes igual
+                    if(dataValida[k].dia < maisAntiga.dia){
                         maisAntiga = dataValida[k];
-                    } else if (dataValida[k].mes == dataValida[k + 1].mes){ // mes igual
-                        if(dataValida[k].dia < dataValida[k + 1].dia){
-                            maisAntiga = dataValida[k];
-                        }
                     }
                 }
-            } cout << maisAntiga.dia << "/" << maisAntiga.mes << "/" << maisAntiga.ano << endl;
+            }
+        } 
+        if(maisAntiga.dia <= 9){
+            cout << "0" << maisAntiga.dia;
+        } else {
+            cout << maisAntiga.dia;
         }
-}
+
+        if(maisAntiga.mes <= 9){
+            cout << "/0" << maisAntiga.mes << "/" << maisAntiga.ano  << endl;   
+        } else {
+            cout << "/" << maisAntiga.mes << "/" << maisAntiga.ano  << endl;  
+        }
+        
+
+        
+        
+    }
+} 
+
 
 int main(){
     Data data[5];
