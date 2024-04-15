4e66ad94d3838bbf12c382785de617f7405ea71b
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2024-04-15 11:53:39 +0000

    aaaaaa

--- 06-RevisaoP1-15-04/ponteiros.cpp
+++ 06-RevisaoP1-15-04/ponteiros.cpp
@@ -7,11 +7,15 @@ int main(){
     int numeros[10] = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10};
     int *pnum = numeros;
 
-    cout << "*(numeros+4): " <<*(numeros+4) << endl;
-    cout << "pnum+4: " << pnum+4 << endl;
+    cout << "pnum+4: " << pnum+4 << endl;  
+    cout << "&pnum+4: " << &pnum+4 << endl;  
+    cout << "&pnum[4]: " << &pnum[4] << endl;
+    cout << "&numeros[4]: " << &numeros[4] << endl;
     cout << "pnum[4]: " << pnum[4] << endl;
     cout << "*(pnum+4): " << *(pnum+4) << endl;
+    cout << "*pnum+4: " << *pnum+4 << endl;
     cout << "numeros[4]: " << numeros[4] << endl;
+    cout << "*(numeros+4): " <<*(numeros+4) << endl;
 
     return 0;
 }
